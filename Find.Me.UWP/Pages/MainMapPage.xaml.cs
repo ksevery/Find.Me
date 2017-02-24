384c779de1b56d82605b35720ecc93ec89bc31a9
ksevery <konstantinsevery@gmail.com>
2017-02-24 22:53:29 +0200

    When a user moves, the map will update along with him and move the pin. Still need to figure out how to handle a smooth transition to the next point.

--- Find.Me.UWP/Pages/MainMapPage.xaml.cs
+++ Find.Me.UWP/Pages/MainMapPage.xaml.cs
@@ -10,6 +10,7 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Storage;
 using Windows.Storage.Streams;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Maps;
@@ -28,9 +29,13 @@ namespace Find.Me.UWP
     /// </summary>
     public sealed partial class MainMapPage : Page
     {
+        private MapIcon centerElement;
+        private Geolocator locator;
+
         public MainMapPage()
         {
             this.InitializeComponent();
+            this.locator = new Geolocator();
         }
 
         private async void mainMap_Loaded(object sender, RoutedEventArgs e)
@@ -41,21 +46,19 @@ namespace Find.Me.UWP
                 case GeolocationAccessStatus.Unspecified:
                     break;
                 case GeolocationAccessStatus.Allowed:
-                    var locator = new Geolocator();
                     var position = await locator.GetGeopositionAsync();
 
                     var center = new Geopoint(new BasicGeoposition { Latitude = position.Coordinate.Point.Position.Latitude, Longitude = position.Coordinate.Point.Position.Longitude });
 
-                    MapIcon centerIcon = new MapIcon();
-                    centerIcon.Location = center;
-                    centerIcon.NormalizedAnchorPoint = new Point(0.5, 1.0);
-                    centerIcon.ZIndex = 0;
-                    centerIcon.CollisionBehaviorDesired = MapElementCollisionBehavior.RemainVisible;
-                    centerIcon.Image = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/MapCircle.png"));
+                    this.centerElement = new MapIcon();
+                    centerElement.Location = center;
+                    centerElement.NormalizedAnchorPoint = new Point(0.5, 1.0);
+                    centerElement.ZIndex = 0;
+                    centerElement.CollisionBehaviorDesired = MapElementCollisionBehavior.RemainVisible;
+                    centerElement.Image = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/MapCircle.png"));
 
                     // Add the MapIcon to the map.
-                    this.mainMap.MapElements.Add(centerIcon);
-                    
+                    this.mainMap.MapElements.Add(centerElement);
 
                     this.mainMap.Center = center;
                     break;
@@ -64,6 +67,29 @@ namespace Find.Me.UWP
                 default:
                     break;
             }
+
+            locator.PositionChanged += Locator_PositionChanged;
+        }
+
+        private async void Locator_PositionChanged(Geolocator sender, PositionChangedEventArgs args)
+        {
+            var position = await sender.GetGeopositionAsync();
+
+            var center = new Geopoint(new BasicGeoposition { Latitude = position.Coordinate.Point.Position.Latitude, Longitude = position.Coordinate.Point.Position.Longitude });
+
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                () =>
+                    {
+                        this.centerElement.Location = center;
+                    
+                        this.mainMap.Center = center;
+                    }
+                );
+        }
+
+        private void mainMap_CenterChanged(MapControl sender, object args)
+        {
+
         }
     }
 }
